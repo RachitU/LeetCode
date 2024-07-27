1b183f0c4c91cb308fa8be18ed87539a06368efa
Crytios <52916791+Crytios@users.noreply.github.com>
2024-07-27 22:57:59 +0530

    Update 0014-longest-common-prefix.java

--- 0014-longest-common-prefix/0014-longest-common-prefix.java
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -5,7 +5,7 @@ class Solution {
         String pre = strs[0];
         for (String r : strs) {
             while (r.indexOf(pre) != 0) {
-
+            //iterates through each string in list and then keeps reducing the prefix till it matches for all 3
                 pre = pre.substring(0, (pre.length() - 1));
 
             }
