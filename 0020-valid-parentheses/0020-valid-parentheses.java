551b84674654f95934b727c7fe94546e32271cb0
Crytios <52916791+Crytios@users.noreply.github.com>
2024-09-05 00:34:38 +0530

    Update 0020-valid-parentheses.java

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -7,6 +7,7 @@ class Solution {
         map.put(']','[');
         for(char i : s.toCharArray()){
             if(map.containsValue(i)) temp.push(i);//put the opening bracket on top(when u push in a stack the element comes on the top)
+            //if ([{}]) then stack in order of {[( at the end of iterations
             else if (map.containsKey(i)) {
                 if( temp.isEmpty() || map.get(i) != temp.pop()) return false;
                 /*to check if element removed from top of stack is the value(opening brack) of the key(closing brack)(pop removes the top element)*/
